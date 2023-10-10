6f7a7cb345c97d4d32c181a6ee027871f81e8a0e
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-10 15:48:17 +0000

    Made pycodestyl and init changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel():
     """The base class"""
     def __init__(self, *args, **kwargs):
@@ -22,9 +23,11 @@ class BaseModel():
                 if (k == "updated_at"):
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, k, v)
-
-        storage.new(self)
-
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """The class string function"""
@@ -34,11 +37,11 @@ class BaseModel():
         """Updates the updated_at field"""
         self.updated_at = datetime.now()
         storage.save()
-        
+
     def to_dict(self):
         """Returns dictionary representation of the class instance"""
         my_dict = {}
-        for k,v in self.__dict__.items():
+        for k, v in self.__dict__.items():
             if (type(v) == datetime):
                 v = v.isoformat()
 
