c882bbc92651f44b605736ca245fefca11cd017f
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-11 11:43:05 +0000

    Merge branch1 to master

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,6 @@ class BaseModel():
     """The base class"""
     def __init__(self, *args, **kwargs):
         """The instantiation method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for k, v in kwargs.items():
                 if (k == "__class__"):
