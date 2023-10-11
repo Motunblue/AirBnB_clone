5ad576868c8fdecd3fa0da0990474ab4a25ebace
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-11 12:10:01 +0000

    The create command of the interpreter

--- console.py
+++ console.py
@@ -3,7 +3,8 @@
     Contains the Console class for the project
 """
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """The command interpreter
@@ -29,6 +30,24 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self, line):
         """continue on encounter with an empty line + ENTER"""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of models classes"""
+        cls_name = arg.split(" ")[0]
+        all_class = {
+                "BaseModel": BaseModel
+                }
+
+        if not cls_name:
+            print("** class name missing **")
+            return
+
+        if cls_name not in all_class:
+            print("** class doesn't exist **")
+            return
+
+        obj = all_class[cls_name]()
+        obj.save()
 
 
 if __name__ == '__main__':
