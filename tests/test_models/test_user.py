227325171d611b5ecb223d4de985c38773e11386
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-14 13:20:18 +0000

    Instantiation Test

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Unittests for module models/user,py
+"""
+from models.user import User
+from models.base_model import BaseModel
+
+class TestUserInstantiation(unittest.TestCase):
+    """Test instantiation of User class"""
+    c = User()
