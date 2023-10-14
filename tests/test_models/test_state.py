ab1861ce6542345bde0d97e0bd6d85ea72eaf643
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-14 13:13:23 +0000

    State Instantiation

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Unittests for module models/city,py
+"""
+from models.state import State
+from models.base_model import BaseModel
+
+class TestStateInstantiation(unittest.TestCase):
+    """Test instantiation of State class"""
+    c = State()
