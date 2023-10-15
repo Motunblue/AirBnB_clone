74eac4d964b9f3565417b7033a0976bf0834a338
Motunrayo <ajirotutu4real@gmail.com>
2023-10-15 13:55:02 +0000

    Modified test_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,10 +12,18 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+import json
+import os
 
 
 class TestFileStorage(unittest.TestCase):
     """Unittest for file storage"""
+
+    def tearDown(self):
+        """Rest json file"""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
     def test_instantiation(self):
         s = FileStorage()
@@ -27,7 +35,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(type(s.__class__._FileStorage__objects), dict)
         self.assertTrue(type(s.__class__._FileStorage__file_path), str)
 
-    def test_save_and_all(self):
+    def test_new_and_all(self):
         a = Amenity()
         b = BaseModel()
         c = City()
@@ -58,3 +66,53 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(type(dict[f"State.{s.id}"]), "State")
         self.assertIn(f"User.{u.id}", dict.keys())
         self.assertTrue(type(dict[f"User.{u.id}"]), "User")
+
+    def test_save(self):
+        a = Amenity()
+        b = BaseModel()
+        c = City()
+        p = Place()
+        r = Review()
+        s = State()
+        u = User()
+        storage.new(a)
+        storage.new(b)
+        storage.new(c)
+        storage.new(p)
+        storage.new(p)
+        storage.new(r)
+        storage.new(s)
+        storage.new(u)
+        storage.save()
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            with open(FileStorage._FileStorage__file_path, "r") as f:
+                json_dict = json.load(f)
+        self.assertIn(f"Amenity.{a.id}", json_dict.keys())
+        self.assertIn(f"BaseModel.{b.id}", json_dict.keys())
+        self.assertIn(f"City.{c.id}", json_dict.keys())
+        self.assertIn(f"Place.{p.id}", json_dict.keys())
+        self.assertIn(f"Review.{r.id}", json_dict.keys())
+        self.assertIn(f"State.{s.id}", json_dict.keys())
+        self.assertIn(f"User.{u.id}", json_dict.keys())
+
+    def test_reload(self):
+        dic = storage.all()
+        self.assertFalse(dic)
+
+        a = Amenity()
+        b = BaseModel()
+        c = City()
+        p = Place()
+        r = Review()
+        s = State()
+        u = User()
+
+        storage.reload()
+
+        self.assertIn(f"Amenity.{a.id}", dic.keys())
+        self.assertIn(f"BaseModel.{b.id}", dic.keys())
+        self.assertIn(f"City.{c.id}", dic.keys())
+        self.assertIn(f"Place.{p.id}", dic.keys())
+        self.assertIn(f"Review.{r.id}", dic.keys())
+        self.assertIn(f"State.{s.id}", dic.keys())
+        self.assertIn(f"User.{u.id}", dic.keys())
